8f91d3ee4fbefb3336e1a10b9598441b7574be11
NivenT <nachenjang@gmail.com>
2016-11-23 15:26:44 -0800

    Added some messages

--- pong.sh
+++ pong.sh
@@ -35,7 +35,7 @@ function get_good_windows() {
 	cnt=0
 	for wind; do
 		if [ "$wind" == "$last" ]; then 
-			echo 'here' 
+			continue
 		fi
 
 		get_dimensions $wind w0 h0
@@ -67,6 +67,8 @@ PADDLE_WIDTH=10
 PADDLE_HEIGHT=30
 PADDLE_BUFFER=10
 
+echo 'Checking number of windows...'
+
 # Get IDs of all windows (except for a few we don't want to mess with)
 windows=($(wmctrl -l | grep -v unity | grep -v Xdnd | grep -v Hud | grep -v Desktop | cut -c 1-10))
 
@@ -76,10 +78,17 @@ if [ ${#windows[@]} -lt 13 ]; then
 	exit 0
 fi
 
+echo 'Figuring out which windows are usable...'
+
 # Only use windows which can be arbitrarily resized
 get_good_windows ${windows[@]} good_windows
 
-print_winds ${good_windows[@]}
+if [ ${#good_windows[@]} -lt 13 ]; then
+	echo 'You must have at least 13 good windows to play'
+	echo 'A window is good if it can be resized to any size'
+	echo 'Only' ${#good_windows[@]} 'of your windows are good'
+	exit 0
+fi
 
 # Separate windows into their individual functions
 score1winds=()
@@ -90,34 +99,35 @@ paddle2winds=()
 # Each score number gets 5 windows
 # All extra windows are split roughly evenly between the two paddles
 
-half=$(( ((${#windows[@]}-10))/2 ))
-rest=$((${#windows[@]}-10-$half))
+half=$(( ((${#good_windows[@]}-10))/2 ))
+rest=$((${#good_windows[@]}-10-$half))
 
 for i in $(seq 0 4); do
-	score1winds[i]=${windows[i]}
+	score1winds[i]=${good_windows[i]}
 done
 
 for i in $(seq 0 4); do
-	score2winds[i]=${windows[i+5]}
+	score2winds[i]=${good_windows[i+5]}
 done
 
 for i in $(seq 1 $half); do
-	paddle1winds[i-1]=${windows[i+9]}
+	paddle1winds[i-1]=${good_windows[i+9]}
 done
 
 for i in $(seq 1 $rest); do
-	paddle2winds[i-1]=${windows[i+9+$half]}
+	paddle2winds[i-1]=${good_windows[i+9+$half]}
 done
 
 # "Block comment"
 if [ 1 -eq 0 ]; then
 	print_winds ${windows[@]}
+	print_winds ${good_windows[@]}
 	print_winds ${score1winds[@]}
 	print_winds ${score2winds[@]}
 	print_winds ${paddle1winds[@]}
 	print_winds ${paddle2winds[@]}
 fi
 
-draw_paddle1 35
+echo 'Initializing game...'
 
-cnt=0
+draw_paddle1 35
